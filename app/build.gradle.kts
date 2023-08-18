86562d54cd6e5f455694deca628f008e6b481c58
Ognjen Radojcic <oradojccic@yahoo.com>
2023-08-18 03:50:19 +0200

    Add map, no API key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -52,6 +53,8 @@ android {
 dependencies {
 
     implementation ("androidx.navigation:navigation-compose:2.6.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.maps.android:maps-compose:2.13.0")
 
 
     implementation("androidx.core:core-ktx:1.10.1")
